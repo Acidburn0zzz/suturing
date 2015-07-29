2adb0151ec1bc4015cafa09be1854f416380b6f6
Siddarth Sen <siddarthsen@yahoo.com>
2015-07-29 14:33:55 -0700

    Modified addAllPaths to add external_libraries to the matlab path

--- addAllPaths.m
+++ addAllPaths.m
@@ -1,4 +1,5 @@
-addpath(genpath([pwd filesep 'gptoolbox']))
+% addpath(genpath([pwd filesep 'gptoolbox']))
 addpath(genpath([pwd filesep 'optimizer']))
 addpath(genpath([pwd filesep 'meshes']))
 addpath(genpath([pwd filesep 'example_code']))
+addpath(genpath([pwd filesep 'external_libraries']))
